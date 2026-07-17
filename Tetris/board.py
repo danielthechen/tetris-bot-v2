1ebaf834b1abd3704526e7aa7f0e3ae7a58e4911
Daniel Chen <ddaanniieell.chen@gmail.com>
2026-07-17 16:51:43 +0800

    Update board.py

---
+++ Tetris/board.py
@@ -0,0 +1,14 @@
+import pygame
+
+color_empty = pygame.Color(0,0,0)
+color_background = pygame.Color(255,255,255)
+color_Font = pygame.Color(255,255,255)
+
+BOARD_ROWS = 20
+BOARD_COLUMNS = 10
+HOLD_ROWS = 20
+HOLD_COLUMNS = 6
+
+GRAV = 1
+GRAV_ACCEL = 0.0001
+GRAV_THRESHOLD = 1000
