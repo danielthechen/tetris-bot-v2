7b1104cbfb3cf84b571b13bd1410fba0e95bc41c
Daniel Chen <ddaanniieell.chen@gmail.com>
2026-07-18 00:55:17 +0800

    change

--- Tetris/piece.py
+++ Tetris/piece.py
@@ -1,7 +1,15 @@
 import random
+import numpy as np
 from tetronimoes import shape
 
 class Piece:
     def __init__(self, name):
         self.name = name
         self.shape = shape[name]
+
+    def move(self, move_x, move_y):
+        self.x += move_x
+        self.y += move_y
+
+    def rotate(self, idx):
+        return np.rot90(self.shape, k=idx)
