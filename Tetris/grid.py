4b7507c5359b660da82e50236c4bfafcab64d560
Daniel Chen <ddaanniieell.chen@gmail.com>
2026-07-19 08:42:15 +0800

    new

--- Tetris/grid.py
+++ Tetris/grid.py
@@ -1,4 +1,5 @@
-from board import BOARD_COLUMNS, BOARD_ROWS
+from config import BOARD_COLUMNS, BOARD_ROWS
+from piece import Piece
 
 EMPTY_BLOCK = -1
 
@@ -8,5 +9,41 @@ class Grid:
 
     def can_fit_shape(self, shape, x, y):
         for i, row in enumerate(shape):
-            for j, is_solid in enumerate(row):
+            for j, filled in enumerate(row):
+                if not filled:
+                    continue
+
+                board_x, board_y = x + j, y + i
+
+                #Wall Collision
+                if board_x < 0 or board_x >= BOARD_COLUMNS:
+                    return False
+
+                #Floor Collision
+                if board_y >= BOARD_ROWS:
+                    return False
                 
+                if self.matrix[board_y][board_x] != EMPTY_BLOCK
+                    return False
+
+        return True
+    
+    def place_piece(self, piece: Piece):
+        for i,row in enumerate(piece.shape):
+            for j,filled in enumerate(row):
+                if filled:
+                    self.matrix[i + piece.y][j + piece.x] = piece.name
+
+    def clear_lines(self):
+        cleared_lines = 0
+
+        for i in reversed(range(len(self.matrix))):
+            if all(block != EMPTY_BLOCK for block in self.matrix[i]):
+                cleared_lines += 1
+            elif cleared_lines > 0:
+                self.matrix[i + cleared_lines] = self.matrix[i:]
+
+        for i in range (cleared_lines):
+            self.matrix[i] = [EMPTY_BLOCK] * BOARD_COLUMNS
+        
+        return cleared_lines
