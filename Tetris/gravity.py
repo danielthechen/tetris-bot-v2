9eb38d2c3686efedd4569ebd39f16b4e26b5e583
Daniel Chen <ddaanniieell.chen@gmail.com>
2026-07-23 00:12:39 +0800

    Update gravity.py

---
+++ Tetris/gravity.py
@@ -0,0 +1,18 @@
+from config import GRAV, GRAV_ACCEL, GRAV_THRESH
+
+class Gravity:
+    def __init__(self):
+        self.speed = GRAV
+        self.progress = 0
+
+    def reset_progress(self):
+        self.progress = 0
+
+    def update_progress(self, dt):
+        self.speed += GRAV_ACCEL * dt
+        self.progress += self.speed * dt
+
+        if self.progress >= GRAV_THRESH:
+            return True
+
+        return False 
